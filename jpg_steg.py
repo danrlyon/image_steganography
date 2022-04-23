64f881d386b586c3159f8914852bbaa94e102979
Dan Albeit <danrlyon.github.io@gmail.com>
2022-04-23 14:16:20 -0600

    Update the jpeg steg to get ready to turn in.

--- jpg_steg.py
+++ jpg_steg.py
@@ -1,30 +1,13 @@
 '''JPEG Steganography
 
-    TODO:
-    - Open Image
-    - Access Pixels
-    - Update pixel LSB with text
-    - Read pixel LSB to build text
-
 '''
-
-from cgi import test
 from os.path import exists
-from tracemalloc import start
-from xxlimited import new
 from PIL import Image
 
-start_image = 'jpeg_file_0.jpg'
-# pixels = list(start_image.getdata())
+
 END_OF_MESSAGE = "THAT'S ALL FOLKS!"
+PIXEL_SPACING = 1
 PREPEND_ENCODED = "secret_"
-HIDER_BIT_POS = 3
-SUPER_LONG_STRING = """
-    Writing a super long string to test out this thing already yo.
-    Hoping it works.
-    Let's see how this all goes and if it works that would
-    be sooooo cool and I would really like it.
-"""
 
 
 def decode_string(input_file):
@@ -40,33 +23,27 @@ def decode_string(input_file):
     print(pixels[:10])
     byte_index = 0
     # rgb_index = 0
-    while byte_index < 10000: #len(byte_list):
+    while byte_index < len(byte_list):
         temp_char = int(0)
         for i in range(8):
-            print(f"Starting with {bin(byte_list[byte_index][byte_index % 3])}")
-            print(f"Buffer is {bin(temp_char)}")
-            bit_result = 1 if byte_list[byte_index][byte_index % 3] > 150 else 0
-            print(f"Bit found: {bin(bit_result)}")
-            temp_char += (bit_result << i)
-            byte_index += 1
-            # print(bin(byte_list[byte_index][byte_index % 3]))
-            print(f"New buffer value: {bin(temp_char)}")
-
             # print(f"Starting with {bin(byte_list[byte_index][byte_index % 3])}")
             # print(f"Buffer is {bin(temp_char)}")
-            # bit_result = (byte_list[byte_index][byte_index % 3] >> HIDER_BIT_POS) % 2
+            bit_result = 1 if byte_list[byte_index][byte_index % 3] > 120 else 0
             # print(f"Bit found: {bin(bit_result)}")
-            # temp_char = temp_char << 1
-            # temp_char += bit_result
-            # byte_index += 1
-            # # print(bin(byte_list[byte_index][byte_index % 3]))
+            temp_char += (bit_result << i)
+            byte_index += PIXEL_SPACING
+            # print(bin(byte_list[byte_index][byte_index % 3]))
             # print(f"New buffer value: {bin(temp_char)}")
+
         result.append(temp_char)
+        # print(result.decode('utf-8'))
         if  result.decode('utf-8')[-len(END_OF_MESSAGE):] == END_OF_MESSAGE:
             break
         # print(result)
-    print(result.decode('utf-8'))
-
+    print("\nSuper secret message found!\n")
+    print("*---------------------------------------------------*")
+    print(result.decode('utf-8')[:-len(END_OF_MESSAGE)])
+    print("*---------------------------------------------------*")
 
 def hide_bits(pixels, message):
     '''Update the LSB of each byte in the pixel'''
@@ -80,44 +57,22 @@ def hide_bits(pixels, message):
     message = [ord(c) for c in f"{message}{END_OF_MESSAGE}"]
     pixel_index = 0
     for byte_letter in message:
-        print(bin(byte_letter))
+        # print(bin(byte_letter))
         for _ in range(8):
             bit = byte_letter % 2
             # Alternate R->G->B values for each bit
-            rgb_index = pixel_index % HIDER_BIT_POS
-            img_bit = (temp[pixel_index][rgb_index] & (2 ** HIDER_BIT_POS)) >> HIDER_BIT_POS
-            print(f"Bit: {bit}, Image Bit: {img_bit}")
-            print(f"Current value to update: {bin(temp[pixel_index][rgb_index])}")
+            rgb_index = pixel_index % 3
+            # img_bit = (temp[pixel_index][rgb_index] & (2 ** HIDER_BIT_POS)) >> HIDER_BIT_POS
+            # print(f"Bit: {bit}, Image Bit: {img_bit}")
+            # print(f"Current value to update: {bin(temp[pixel_index][rgb_index])}")
             if bit:
-                temp[pixel_index][rgb_index] = 200
+                temp[pixel_index][rgb_index] = 140
             else:
                 temp[pixel_index][rgb_index] = 100
-            print(temp[pixel_index])
-            pixel_index += 1
+            # print(temp[pixel_index])
+            pixel_index += PIXEL_SPACING
             byte_letter = byte_letter >> 1
-            # if bit == img_bit:
-            #     print(f"Bits are equal, no change.")
-            # if bit:
-            #     temp[pixel_index][rgb_index] |= ((2 ** (HIDER_BIT_POS + 1)) - 1)
-                
-            # else:
-            #     temp[pixel_index][rgb_index] &= ((2 ** 8) - 1) - (2 ** HIDER_BIT_POS)
-            
-            # temp[pixel_index][rgb_index] |= (2 ** (HIDER_BIT_POS -1))
-            # temp[pixel_index][rgb_index] -= (2 ** (HIDER_BIT_POS-2) - 1)
-            # print(f"Bit set to {bit}, new value: {bin(temp[pixel_index][rgb_index])}")
-            # pixel_index += 1
-            # byte_letter = byte_letter >> 1
 
-            # bit = byte_letter % 2
-            # if temp[pixel_index] % 2 != bit:
-            #     if temp[pixel_index] < 200:
-            #         temp[pixel_index] += 1
-            #     else:
-            #         temp[pixel_index] -= 1
-            # pixel_index += 1
-            # byte_letter = byte_letter >> 1
-    
     result = list()
     pixel_count = len(pixels)
     current_pixel = 0
@@ -143,10 +98,10 @@ def encode_string(input_file):
     writeable_bytes = pixel_count * 3
     print(f"Maximum message size: {writeable_bytes}")
     # TODO: calculate maximum message length based on image
-    # input_string = ""
-    # while len(input_string) < 1:
-    #     input_string = input("Enter message to hide in image: ")
-    encoded_pixels = hide_bits(pixels, SUPER_LONG_STRING)
+    input_string = ""
+    while len(input_string) < 1:
+        input_string = input("Enter message to hide in image: ")
+    encoded_pixels = hide_bits(pixels, input_string)
     i, j = 0, 0
     for pixel in encoded_pixels:
         new_image.putpixel((i, j), pixel)
@@ -155,39 +110,32 @@ def encode_string(input_file):
             j += 1
         else:
             i += 1
-    # print(new_image.getpixel((0, 0)))
-    # print(new_image.load()[0, 0], new_image.getdata()[1], new_image.getdata()[2])
     new_filename = f"{PREPEND_ENCODED}{input_file}"
     new_image.save(
             new_filename,
             format="JPEG",
             quality=95,
-            subsampling=0)#,
-            # dpi=(new_image.size[0],
-            # new_image.size[1]))
-    test_image = Image.open(new_filename, 'r')
-    
+            subsampling=0)
 
 
 def main():
     '''Main Function'''
-    # option_1 = input("Select option (encode or decode): ").lower()
-    # while option_1 != "encode" and option_1 != "decode":
-    #     print(f"Invalid Selection: {option_1}")
-    #     option_1 = input("Select option (encode or decode): ").lower()
+    option_1 = input("Select option (encode or decode): ").lower()
+    while option_1 != "encode" and option_1 != "decode":
+        print(f"Invalid Selection: {option_1}")
+        option_1 = input("Select option (encode or decode): ").lower()
 
-    # input_file = input(f"Select file to {option_1}:")
-    # while not exists(input_file):
-    #     print(f"The file, {input_file} does not exist.")
-    #     input_file = input(f"Select file to {option_1}: ")
-    # TODO: validate jpeg
-    # if option_1 == "encode":
-    #     encode_string(input_file)
-    # elif option_1 == "decode":
-    #     decode_string(input_file)
-    encode_string(start_image)
-    decode_string(f'{PREPEND_ENCODED}{start_image}')
-    print("\nAll done! Good job!")  
+    input_file = input(f"Select file to {option_1}: ")
+    while not exists(input_file) or "jpg" not in input_file.lower():
+        print(f"The file, {input_file} does not exist, or is not a JPEG.")
+        input_file = input(f"Select file to {option_1}: ")
+
+    if option_1 == "encode":
+        encode_string(input_file)
+    elif option_1 == "decode":
+        decode_string(input_file)
+
+    print("\nAll done! Good job!")
 
 
 if __name__ == '__main__':
